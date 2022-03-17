54ede837bbf58019091024b93a3816ea21a2366e
Daniel Wan Jonathan <daniel01.jonathan@gmail.com>
2022-03-17 16:02:58 +0800

    Included txt in gitignore

--- aims/email_fetcher/schedule_control.py
+++ aims/email_fetcher/schedule_control.py
@@ -1,20 +1,21 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from pytz import timezone
+from EmailBackend import EmailBackend
 
 #This method is for testing, we will be importing the run_backend from EmailBackend.py
 def sendMessage():
-    print('Yes')
+    return EmailBackend.run_backend
 
 sched = BackgroundScheduler(timezone="Asia/Singapore")
 sched.start()
 
 #Add jobs(Basically schedule when to run script)
-job1 = sched.add_job(sendMessage,'cron',hour="15", minute="21") #(method,timertype,hour,minute)
-job2 = sched.add_job(sendMessage,'cron',hour="15", minute="22")
-job3 = sched.add_job(sendMessage,'cron',hour="15", minute="23")
+job1 = sched.add_job(sendMessage,'cron',hour="16", minute="01") #(method,timertype,hour,minute)
+job2 = sched.add_job(sendMessage,'cron',hour="16", minute="02")
+job3 = sched.add_job(sendMessage,'cron',hour="16", minute="03")
 
-#Just printing the job to see whether
+#Just printing the job to see whether the job is created
 print(job1)
 print(job2)
 print(job3)
