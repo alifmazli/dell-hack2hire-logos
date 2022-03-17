7b959ac41141008e02887c42da3d3c33a23f72d5
Alif <alifmazli01@gmail.com>
2022-03-17 18:26:11 +0800

    fixed EmailBackend class call

--- aims/email_fetcher/views.py
+++ aims/email_fetcher/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .EmailBackend import email_backend
+from .EmailBackend import EmailBackend
 from apscheduler.schedulers.background import BackgroundScheduler
 
 #Timer to schedule when to get attachment. Don't touch
@@ -7,22 +7,26 @@ sched = BackgroundScheduler(timezone="Asia/Singapore")
 @sched.scheduled_job('cron',hour="08", minute="00", second='00')
 def timed_job():
     print('Downloaded attachment')
-    run = email_backend.run_email_backend()
+    run = EmailBackend()
+    run.run_email_backend()
 
 @sched.scheduled_job('cron',hour="12", minute="00", second='00')
 def timed_job():
     print('Downloaded attachment')
-    run = email_backend.run_email_backend()
+    run = EmailBackend()
+    run.run_email_backend()
 
-@sched.scheduled_job('cron',hour="17", minute="00", second='00')
+@sched.scheduled_job('cron',hour="18", minute="24", second='30')
 def timed_job():
     print('Downloaded attachment')
-    run = email_backend.run_email_backend()
+    run = EmailBackend()
+    run.run_email_backend()
+
 sched.start()
 
+""""
 #For testing purposes, you can mess with this code
-"""
-@sched.scheduled_job('interval', second=10)
+@sched.scheduled_job('interval', seconds=10)
 def timed_job():
     print('Downloaded attachment')
     run = email_backend.run_email_backend()
